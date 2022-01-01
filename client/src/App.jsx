d61691af0e4bfe6b411f98a0ca61ebca226c9d93
apoorvasa03 <apoorvasa03@gmail.com>
2022-01-01 11:33:26 -0800

    - product page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,26 @@ import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
 
+import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+
 const App = () => {
-  return <Home/>;
+  let user = true;
+  return(
+    
+    <div className="App">
+    <Router>
+      <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/products/:category' element= {<ProductList />}/>
+          <Route path='/product/:id' element={ <Product />}></Route>
+          <Route path='/' element={<Home />}/>
+          <Route path='/register' element={user ? <Navigate to="/"/> : <Register/> }/>
+          <Route path='/login' element= {user ? <Navigate to="/"/> : <Login/> }/>
+          <Route path='/cart' element={<Cart />}/>
+      </Routes>
+    </Router>
+  </div>
+  )
 };
 
 export default App;

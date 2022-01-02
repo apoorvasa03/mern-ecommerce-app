ca778037112ff82bda0ccfa99cd942fc330532e1
apoorvasa03 <apoorvasa03@gmail.com>
2022-01-02 01:12:57 -0800

    - user and order integration

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = 'http://localhost:5000/api/'
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxY2Y3Nzc4ZTE2ZmE5ZWM4MDM0YWMwMSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0MTA3MTM1OSwiZXhwIjoxNjQxMzMwNTU5fQ.yh0TIB9GuXVnVW60l1h2K1vT5R0ejjyLE8eeQCcBPjk'
-
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL
